9e9549cfa9dfefb01b94e9a345913f7df8f8a2e8
RoMANzhula <roman1985al@ukr.net>
2024-10-21 15:52:03 +0300

    Add FindNumberInSortedArray example - add description

--- src/algorithms/bynary_search/FindNumberInSortedArray.java
+++ src/algorithms/bynary_search/FindNumberInSortedArray.java
@@ -34,5 +34,22 @@ public class FindNumberInSortedArray {
 // We use binary search because the array is sorted, and binary search has logarithmic complexity O(log n), which
 // allows us to quickly find elements.
 
+//When to use:
+//- When you want to find an element in a sorted array or range.
+//- Tasks for searching where elements are in ascending or descending order.
+//Example tasks:
+//- Search for the index of the given number in the sorted array.
+//- Search for the first or last occurrence of an element in an array.
+//Time complexity: O(log n).
+
+
 // Ми використовуємо бінарний пошук, оскільки масив відсортований, і бінарний пошук має логарифмічну
 // складність O(log n), що дозволяє швидко знаходити елементи.
+
+//Коли використовувати:
+//- Коли потрібно знайти елемент у відсортованому масиві або діапазоні.
+//- Задачі на пошук, де елементи в порядку зростання чи спадання.
+//Приклад задач:
+//- Пошук індексу заданого числа у відсортованому масиві.
+//- Пошук першої або останньої появи елемента в масиві.
+//Часова складність: O(log n).
